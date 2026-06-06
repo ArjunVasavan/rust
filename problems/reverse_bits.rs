ca60a458a14e4cb38a11ad58abf8f1a2d49ecb61
Arjun Vasavan <vasavanarjun@gmail.com>
2026-06-06 21:21:54 +0530

    🦀

---
+++ problems/reverse_bits.rs
@@ -0,0 +1,14 @@
+// @leet start
+impl Solution {
+    pub fn reverse_bits(n: i32) -> i32 {
+        let mut n = n as u32 ;
+        n = ((n & 0xFFFF0000) >> 16) | ((n & 0x0000FFFF) << 16);
+        n = ((n & 0xFF00FF00) >>  8) | ((n & 0x00FF00FF) <<  8);
+        n = ((n & 0xF0F0F0F0) >>  4) | ((n & 0x0F0F0F0F) <<  4);
+        n = ((n & 0xCCCCCCCC) >>  2) | ((n & 0x33333333) <<  2);
+        n = ((n & 0xAAAAAAAA) >>  1) | ((n & 0x55555555) <<  1);
+        n as i32
+    }
+}
+// @leet end
+

856c155f332413de99d4c4f7659cd4cf3ce32808
Arjun Vasavan <vasavanarjun@gmail.com>
2026-06-21 00:05:08 +0530

    🦀

--- basics/datatypes/fixed_sized_types.rs
+++ basics/datatypes/fixed_sized_types.rs
@@ -1,4 +1,14 @@
+ /* an function that wheen we add negative returns error */
+
+fn get_default_age()->i8 {
+    return -1;
+}
+
 fn main() {
     let a: u32 = 25;
-    println!("{}",a);
+    let b: u8 = 64;
+    println!("unsigned 32 bit {}",a);
+    println!("unsigned 8 bit {}",b);
+    let age: i8 = get_default_age(); /* if you add u8 inspite of i8 its an error */
+    println!("age is {}",age);
 }
